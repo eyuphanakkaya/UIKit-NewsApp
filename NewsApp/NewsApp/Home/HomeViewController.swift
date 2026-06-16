4ef6ad081444dc02fe30149b9a296c4668ed3889
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-16 18:28:08 +0300

    Add HomeViewController and HomeViewModel
    Add HomeViewController (initialized with HomeViewModel) and HomeViewModel; remove the old ViewController. HomeViewController triggers an async load in viewWillAppear. HomeViewModel (annotated @MainActor) depends on FeedLoader and manages news, isLoading and errorMessage states. Update Main.storyboard to reference HomeViewController, bump tools/plugin versions, add a systemBackgroundColor resource, and adjust the view frame/background color.

---
+++ NewsApp/NewsApp/Home/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Eyüphan Akkaya on 16.06.2026.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    private let viewModel: HomeViewModel
+    
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Task {
+            await viewModel.load()
+        }
+    }
+
+
+}
+
---
+++ NewsApp/NewsApp/Home/HomeViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewModel.swift
+//  NewsApp
+//
+//  Created by Eyüphan Akkaya on 16.06.2026.
+//
+
+import Foundation
+
+@MainActor
+final class HomeViewModel {
+    private let loader: FeedLoader
+    
+    private(set) var news: [NewsModel] = []
+    private(set) var isLoading: Bool = false
+    private(set) var errorMessage: String = ""
+    
+    init(loader: FeedLoader) {
+        self.loader = loader
+    }
+    
+    
+    func load() async {
+        isLoading = true
+        
+        defer { isLoading = false }
+        
+        do {
+            news = try await loader.load()
+            print(news, "EYUPHAN")
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+    }
+    
+}
