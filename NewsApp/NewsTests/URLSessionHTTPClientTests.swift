b3f472e07c5cc9ae04415b2526edde9d75458708
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-06-22 12:18:14 +0300

    create URLSessionHTTPClient test and than test perform get request with url

---
+++ NewsApp/NewsTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,84 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  NewsTests
+//
+//  Created by Eyüphan Akkaya on 22.06.2026.
+//
+
+import XCTest
+import NewsApp
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_performGETRequestWithURL() async throws {
+        let url = URL(string: "https://any-url.com")!
+        let sut = makeSUT()
+        
+        URLProtocolStub.stub(
+            data: Data(),
+            response: HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil),
+            error: nil)
+        
+        _ = try await sut.get(url: url)
+        
+        XCTAssertEqual(URLProtocolStub.lastRequest?.url, url)
+    }
+    
+    
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHTTPClient(session: session)
+        return sut
+    }
+    
+    private final class URLProtocolStub: URLProtocol {
+        private static var stub: Stub?
+        static var lastRequest: URLRequest?
+        
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            request
+        }
+        
+        override func startLoading() {
+            URLProtocolStub.lastRequest = request
+            
+            if let data = URLProtocolStub.stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = URLProtocolStub.stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+                    
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() {}
+
+        
+    }
+
+}
